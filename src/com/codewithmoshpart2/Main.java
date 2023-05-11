f9ece557afb5510cbee4a475889c8c0c5185acc8
Mosh Hamedani <130055118+98ganesham@users.noreply.github.com>
2023-05-11 17:30:59 +0630

    The Ultimate DataStructure Part2

---
+++ src/com/codewithmoshpart2/Main.java
@@ -0,0 +1,12 @@
+package com.codewithmosh1;
+public class Main{
+    public static void main(String []  args){
+        AllTree tree = new AllTree();
+        tree.insert(20);
+        tree.insert(30);
+        tree.insert(40);
+        tree.insert(10);
+        tree.insert(50);
+        System.out.println("Done");
+    }
+}
