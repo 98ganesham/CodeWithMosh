dd95efdb67768952746172be15b7bbce0a0affe4
Mosh Hamedani <130055118+98ganesham@users.noreply.github.com>
2023-05-15 09:19:48 +0630

    codewithmoshpart2

---
+++ src/com/codewithmoshpart2/Heap/Main.java
@@ -0,0 +1,15 @@
+package com.codewithmosh1;
+
+    public class Main{
+        public static void main(String[] args) {
+            var heap = new Heaps();
+            heap.insert(10);
+            heap.insert(5);
+            heap.insert(17);
+            heap.insert(4);
+            heap.insert(22);
+            heap.remove();
+            System.out.println("Done");
+
+    }
+}
