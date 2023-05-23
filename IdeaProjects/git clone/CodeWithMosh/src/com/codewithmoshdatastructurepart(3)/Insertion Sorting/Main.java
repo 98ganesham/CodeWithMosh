ee329c8ae2ebf989309c4b84e8637c5f35e8b2af
Mosh Hamedani <130055118+98ganesham@users.noreply.github.com>
2023-05-23 08:43:45 +0630

    CodeWithMosh

--- IdeaProjects/git clone/CodeWithMosh/src/com/codewithmoshdatastructurepart(3)/Insertion Sorting/Main.java
+++ IdeaProjects/git clone/CodeWithMosh/src/com/codewithmoshdatastructurepart(3)/Insertion Sorting/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main (String [] names){
         int [] numbers ={7,3, 5, 2, 1, 2 ,5, 8};
-        var sorter = new MergeSort();
+        var sorter = new InsertionSort();
         sorter.sort(numbers);
         System.out.println(Arrays.toString(numbers));
     }
